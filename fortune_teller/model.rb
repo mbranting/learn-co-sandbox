63f137e0db75fbaa85ce335e1d1129d2885f9bbd
mbranting <mckenna.branting@gmail.com>
2018-06-13 21:45:38 +0000

    automatically backed up by learn

---
+++ fortune_teller/model.rb
@@ -0,0 +1,4 @@
+def get_fortune
+  fortunes_array = ["You will find love", "A big opportunity awaits", "You have secret admirer"]
+  return fortunes_array.sample
+end
---
+++ quiz.rb
@@ -0,0 +1,6 @@
+#How eco-friendly are you?
+puts "How eco-friendly are you?"
+start = start_button
+puts "Ready to #{start}?"
+if #person clicks on the start button, continue
+puts "1. Do you "
